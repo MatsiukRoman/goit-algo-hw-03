1c1496150816930202737ddd70e3ebc3b46a11a3
MatsiukRoman <r.matsuk@gmail.com>
2024-04-21 12:38:44 +0300

    fixing error incorect values

--- task2.py
+++ task2.py
@@ -5,6 +5,12 @@ def get_numbers_ticket(min:int, max:int, quantity:int)->list:
         if min<1 or max >1000:
             print("The minimum possible number in the set (at least 1) and the maximum possible number (at most 1000).")
             return list()
+        elif (max-min)+1 < quantity:
+            print(f"The range of the random sequence {(max-min)+1} is less than the value {quantity} of winning numbers!")
+            return list()
+        elif quantity <= 0 :
+            print("Enter the positive value of the winning numbers!")
+            return list()   
         else:
             result_array = set()
             while len(result_array) != quantity:
@@ -16,7 +22,7 @@ def get_numbers_ticket(min:int, max:int, quantity:int)->list:
         return list()
 
 try:
-    lottery_numbers = get_numbers_ticket(1,999,6)
+    lottery_numbers = get_numbers_ticket(10,14,6)
     print("Ваші лотерейні числа:", lottery_numbers)
 except Exception as e:
     print("Exception: ", e)
